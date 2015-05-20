347222ae9fff0714c5cc1b34108b15b2f4a5b4ef
Remco <remcosannen@gmail.com>
2015-05-20 14:56:44 +0200

    Added scoresystem to Pong and fixed some bugs.
    Added a scoresystem, placed the startposition of the ball in the middle
    and changed the collision.

--- games/Pong/Pong.cpp
+++ games/Pong/Pong.cpp
@@ -13,6 +13,11 @@
 
 #include <glm/gtc/matrix_transform.hpp>
 
+GameBase* getGame()
+{
+	return new Pong();
+}
+
 std::string Pong::getName()
 {
 	return "Pong";
@@ -20,39 +25,35 @@ std::string Pong::getName()
 
 std::string Pong::getInstructions()
 {
-	return "Reflect the balls";
+	return "Reflect the ball";
 }
 
 void Pong::loadResources()
 {
 	backSprite = resourceManager->getResource<blib::Texture>("assets/games/Pong/back.png");
 	playerSprite = resourceManager->getResource<blib::Texture>("assets/games/Pong/rectangle.png");
-	trashSprite = resourceManager->getResource<blib::Texture>("assets/games/Pong/Ball.png");
+	ballSprite = resourceManager->getResource<blib::Texture>("assets/games/Pong/Ball.png");
 	font = resourceManager->getResource<blib::Font>("menu");
-
 }
 
 void Pong::start(Difficulty difficulty)
 {
-	trashCount = 1;
 	positive = false;
 	speed = 2;
 	turningFactor = 0;
-	trash.clear();
+	ball.clear();
+	rotation = rand();
 
 	maxPlayerScore = 5;
 
 	for (auto p : players)
 	{
 		p->position = glm::vec2(1920 / 2, 1080 / 2) + 500.0f * blib::util::fromAngle(p->index / (float)players.size() * 2 * (float)M_PI);
-		p->rotation = (float)M_PI / 4 * turningFactor;
+		p->rotation = (float)M_PI / (float)players.size()*2 * turningFactor;
 		turningFactor++;
 	}
-	glm::vec2 v(0, 0);
-	while (!blib::linq::any(players, [this, &v](PongPlayer* p) { return glm::length(p->position - (v = glm::vec2(blib::math::randomFloat(100.0f, 1920 - 200.0f), blib::math::randomFloat(100.0f, 1080 - 200.0f)))) > 600.0f; })) {}
-	trash.push_back(v);
-    
-
+	glm::vec2 v(1920/2, 1080/2);
+	ball.push_back(v);
 }
 
 void Pong::update(float elapsedTime)
@@ -60,6 +61,13 @@ void Pong::update(float elapsedTime)
 	blib::math::Rectangle screenRect(0, 0, 1920, 1080);
 	for (auto p : players)
 	{
+		if (!screenRect.contains(ball[0]))
+		{
+			p->score += 1;
+			ball[0] = glm::vec2(1920 / 2, 1080 / 2);
+			speed = 2;
+			rotation = rand();
+		}
 		if (p->joystick.leftStick.y < 0)
 		{
 			glm::vec2 oldPosition = p->position;
@@ -109,44 +117,22 @@ void Pong::update(float elapsedTime)
 			}
 		}
 		
-			
-
 		glm::vec2 suckPos = p->position + 50.0f*blib::util::fromAngle(p->rotation);
 		
-		if (blib::linq::any(trash, [p](glm::vec2 t) { return glm::length(t - p->position) < 64; }))
+		if (blib::linq::any(ball, [p](glm::vec2 t) { return glm::length(t - p->position) < 64; }))
 		{
-			speed += 1;
-			if (!positive)
-			{
-				positive = true;
-			}
-			else
-			{
-				positive = false;
-			}
+			speed += 0.5;
+			rotation -= 1;
 		}
-		if (glm::length(trash[0] - p->position) < 50)
+		if (glm::length(ball[0] - p->position) < 50)
 		{
-			trash[0].x -= 20 * elapsedTime;
+			ball[0].x -= 20 * elapsedTime;
 		}
 		else
 		{
-			if (!positive)
-			{
-				trash[0].x += 25 * speed * elapsedTime;
-			}
-			else
-			{
-				trash[0].x -= 25 * speed * elapsedTime;
-			}
+			ball[0] += speed * 20.0f * blib::util::fromAngle(rotation) * elapsedTime;
 		}
-			
-
 	}
-	
-	//plaatsen
-	
-	
 }
 
 
@@ -155,7 +141,7 @@ void Pong::draw()
 	spriteBatch->begin(settings->scaleMatrix);
 	spriteBatch->draw(backSprite, glm::mat4());
 
-	for (auto t : trash) { spriteBatch->draw(trashSprite, blib::math::easyMatrix(t), trashSprite->center); };
+	for (auto t : ball) { spriteBatch->draw(ballSprite, blib::math::easyMatrix(t), ballSprite->center); };
 	for (auto p : players) { spriteBatch->draw(playerSprite, glm::rotate(glm::translate(glm::mat4(), glm::vec3(p->position, 0)), glm::degrees(p->rotation), glm::vec3(0, 0, 1)), playerSprite->center, blib::math::Rectangle(0, 0, 1, 1), p->participant->color); }
 	for (size_t i = 0; i < players.size(); i++)
 		spriteBatch->draw(font, blib::util::toString(players[i]->score), blib::math::easyMatrix(glm::vec2(10, 64 * i), 0, 1), players[i]->participant->color);
--- games/Pong/Pong.h
+++ games/Pong/Pong.h
@@ -9,16 +9,16 @@ namespace blib { class Texture; class Font; }
 class Pong : public ScoreGame<PongPlayer>
 {
 	blib::Texture* backSprite;
-	blib::Texture* trashSprite;
+	blib::Texture* ballSprite;
 	blib::Texture* playerSprite;
 	blib::Font* font;
 
 	boolean positive;
-	int trashCount;
+	double rotation;
 	int turningFactor;
-	int speed;
+	float speed;
 
-	std::vector<glm::vec2> trash;
+	std::vector<glm::vec2> ball;
 
 public:
 	virtual std::pair<int, int> getPlayerCount() { return std::pair<int, int>(1, 100); }
@@ -33,3 +33,10 @@ public:
 
 
 
+
+
+
+
+extern "C" {
+	__declspec(dllexport) GameBase* getGame();
+}
